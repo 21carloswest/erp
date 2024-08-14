622305633a1cd5a510ab13434be5b22d646fc9bc
Carlos <c4rl.edu4rdo@gmail.com>
2024-08-14 14:32:25 -0300

    Adição da chave estrangeira na tabela transportadora

--- database/migrations/2024_08_12_160019_create_transportadoras_table.php
+++ database/migrations/2024_08_12_160019_create_transportadoras_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Empresa;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('transportadoras', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Empresa::class, 'empresaId');
             $table->string('cnpjCpf', 14);
             $table->string('nomeRazao', 70);
             $table->string('inscricaoEstadual', 14);
